398f82a5dc280803e44a7e021307bcb33812acb7
Pharadogz <pharadogz@gmail.com>
2024-12-05 15:51:45 +0300

    Init

--- src/masks.py
+++ src/masks.py
@@ -1,4 +1,21 @@
+def get_mask_card(card_number: str) -> str:
+    """функция, маскировки номера карты"""
+    new_list = list()
+    new_list.append(card_number[0:4])
+    new_list.append(card_number[4:6] + "**")
+    new_list.append("****")
+    new_list.append(card_number[12:])
+    return "".join(new_list)
+
+
 from typing import Any
+
+
+def get_mask_account(acc_number: str) -> str:
+    """Функция, маскировки номера счета"""
+    new_list = list()
+    new_list.append("**" + acc_number[-4:])
+    return "".join(new_list)
 
 
 def get_mask_card_number(card_number: Any) -> str:
@@ -11,6 +28,10 @@ def get_mask_card_number(card_number: Any) -> str:
     raise ValueError("Некорректный номер карты")
 
 
+# client_acc_number = 73654108430135874305
+# client_card_number = 7000792289606361
+# print(get_mask_account(str(client_acc_number)))
+# print(get_mask_card(str(client_card_number)))
 def get_mask_account(account_number: Any) -> str:
     """Принимет номер счета, возвращает маску счета в типе str"""
     account_number = str(account_number)
@@ -18,4 +39,4 @@ def get_mask_account(account_number: Any) -> str:
     if account_number.isdigit() and len(account_number) == 20:
         masked_account = "**" + account_number[-4:]
         return str(masked_account)
-    raise ValueError("Некорректный номер счета")
+    raise ValueError("Некорректный номер счёта")
--- src/processing.py
+++ src/processing.py
@@ -3,7 +3,7 @@ from datetime import datetime
 
 def filter_by_state(dict_list: list, state_value: str = "EXECUTED") -> list:
     """Принимает список словарей и опционально значение для ключа state. Возвращает новый список словарей, содержащий
-    только те, у которых ключ state соответствует указанному значению (по умолчанию 'EXECUTED')"""
+ только те, у которых ключ state соответствует указанному значению (по умолчанию 'EXECUTED')"""
     returned_list = []
     for dict_ in dict_list:
         if dict_["state"] == state_value:
@@ -13,8 +13,7 @@ def filter_by_state(dict_list: list, state_value: str = "EXECUTED") -> list:
 
 def sort_by_date(dict_list: list, sort_by_date_descending: bool = True) -> list:
     """Принимает список словарей. Возвращает новый список, отсортированный по дате от новых к старым. Если надо
-    изменить порядок сортировки, то при вызове функции вторым параметром передай False
-    :rtype: object"""
+ изменить порядок сортировки, то при вызове функции вторым параметром передай False"""
     sorted_list = sorted(
         dict_list, key=lambda strindate: datetime.fromisoformat(strindate["date"]), reverse=sort_by_date_descending
     )
